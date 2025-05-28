4f519fc877953e7d52abadc6d7df769b42c23ce6
Zadoya <zadoyamaxim@gmail.com>
2025-05-28 23:13:08 +0200

    a3x1, a3x2, a3x3 komplett funktionsfähig

--- src/kw21/required/a3x2/cardProcessor/CardProcessor.java
+++ src/kw21/required/a3x2/cardProcessor/CardProcessor.java
@@ -4,6 +4,7 @@ package kw21.required.a3x2.cardProcessor;
 import static kw21.required.a3x2.cards.Card.*;
 
 import static kw21.required.a3x2.cards.Card.Constant.*;
+
 //
 import kw21.required.a3x2.cards.*;
 import kw21.required.a3x2.cards.Card.*;
@@ -13,16 +14,65 @@ import kw21.required.a3x2.cards.Card.*;
 /**
  * CardProcessor - sort cards in-place - see task
  * 
- * @author  (your name(s)) 
- * @version (a version number or a date)
+ * @author  Maksym Zadoya
+ * @version 28.05.2025 v1.ß
  */
 public class CardProcessor {
     
-    public void sortCards( final Card[] cardsToBeSorted ){
-        // TODO ...
-    }//method()
-    Card[][] generateCardMatrix (Card[] givenCards) {
+  
+    
+        public static Card[][] generateCardMatrix (Card[] givenCards) {
+            Card[][] cardMatrix = new Card[4][13];
         
-        return generateCardMatrix;
-    }
+        int lengthOfArray = givenCards.length;
+        
+        for (int i = 0; i < cardMatrix.length; i++) {
+            for(int j = 0; j < cardMatrix[0].length; j++) {
+                for (int h = 0; h < givenCards.length; h++) {
+                final Rank rankCard = givenCards[h].getRank();
+                final Suit suitCard = givenCards[h].getSuit();
+                final int cardSuitAsIndex = suitCard.ordinal();
+                final int cardRankAsIndex = rankCard.ordinal();
+                if (cardSuitAsIndex == i) {
+                   if (cardRankAsIndex == j) {
+                    cardMatrix[i][j] = givenCards[h];   
+                     }//if inside if
+                   }//if 
+                }//for inside for inside for         
+            }//for inside for
+        }//for
+        printCardMatrixForFun(cardMatrix);
+        return cardMatrix;
+    }//method
+        
+        public static void sortCards(Card[] cardArrayToBeSorted) {
+            
+            Card[][] cardMatrix = generateCardMatrix(cardArrayToBeSorted);
+            int posCounter = 0;
+            for (int i = 0; i < 4; i++) {
+                for (int j = 0; j < 13; j++) {
+                    if (cardMatrix[i][j]!=null) {
+                        cardArrayToBeSorted[posCounter] = cardMatrix[i][j];
+                        posCounter++;
+                    }//if 
+                }//for inside for
+            }//for
+        }//method
+        
+        
+        //nicht relevant 
+        
+        public static void printCardMatrixForFun(Card[][] cardMatrix) {
+           
+            for (int i = 0; i < 4; i++) {
+                for (int j = 0; j < 13; j++) {
+                    if (cardMatrix[i][j]!=null) {
+                        System.out.printf("%2s ", cardMatrix[i][j]);
+                    }//if
+                    else System.out.printf(" ####  ");
+                }//for inside for
+                System.out.println();
+            }//for
+            System.out.println();
+        }//method
 }//class
--- src/kw21/required/a3x3/patternBasedArrayProcessor/ArrayProcessor.java
+++ src/kw21/required/a3x3/patternBasedArrayProcessor/ArrayProcessor.java
@@ -11,21 +11,7 @@ package kw21.required.a3x3.patternBasedArrayProcessor;
  */
 public class ArrayProcessor {
     
-    //###
-    //###
-    //###
-    //###         HIER kommt Ihr Code hin ;-)
-    //###
-    //###                    VVVV
-    //###                    VVVV
-    //###                    VVVV
-    //###   VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV
-    //###      VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV
-    //###         VVVVVVVVVVVVVVVVVVVVVVVVVV
-    //###            VVVVVVVVVVVVVVVVVVVV
-    //###               VVVVVVVVVVVVVV
-    //###                  VVVVVVVV
-    //###                     VV
+
     public long process( long[][] theArray ){
         
 
@@ -35,45 +21,91 @@ public class ArrayProcessor {
         int testedFor = musterSiteHeigth + musterSiteLength;
         int startPosition = 0;
         
-        int longestReihe = 0; 
+        int longestReiheMid = 999999999; 
         int longestReiheToCompare = 0;
+        int longestReiheStartEnd = 999999999;
+        int longestReiheBegrenzung = 0;
         
         long sum = 0;
+        System.out.printf("theArray is: %n");
+        for (int i = 0 ; i < theArray.length; i++) {
+            for (int j = 0; j < theArray[i].length; j++) {
+                System.out.printf("%d", theArray[i][j]);
+            }
+            System.out.println();
+        }
         
         while (startPosition+testedFor <= theArray.length) {
             
-           for (int i = startPosition; i<testedFor+startPosition; i++) {
-               longestReiheToCompare = theArray[i].length;
-               if (longestReiheToCompare>longestReihe) { longestReihe = longestReiheToCompare; 
-              }
-           }
-           for (int i = startPosition; i<testedFor+startPosition; i++) {
-               longestReiheToCompare = theArray[i].length;
-               if (longestReiheToCompare<longestReihe) { longestReihe = longestReiheToCompare; 
-              }
-           }
-           
-          // for (int i = startPosition; i < testedFor+startPosition; i++) {
-            for (int j = 0; j < longestReihe-testedFor+1; j++) {
+            if (theArray[startPosition].length > 0 && theArray[startPosition+1].length > 0 && 
+                    theArray[startPosition+2].length > 0 && theArray[startPosition+3].length > 0) {
+                
+                
+                
+            for (int i = startPosition; i<testedFor+startPosition; i++) {
+                
+                if (i - startPosition == 1 || i - startPosition == 2 ) {
+                    longestReiheToCompare = theArray[i].length;
+                      if (longestReiheToCompare<longestReiheMid) { 
+                          longestReiheMid = longestReiheToCompare; 
+                          
+                    }//if inside if
+                      
+                }//if inside if and for
+                else { 
+                    longestReiheToCompare = theArray[i].length;
+                     if (longestReiheToCompare < longestReiheStartEnd) {
+                         longestReiheStartEnd = longestReiheToCompare;
+                    }//if inside else 
+                }//else
+            }//for 
+            
+            if (longestReiheStartEnd >= longestReiheMid) {
+                longestReiheBegrenzung = longestReiheMid;
+            }//if 
+            else if (longestReiheStartEnd < longestReiheMid) {
+                longestReiheBegrenzung = longestReiheStartEnd+1; 
+            }
+            
+            System.out.printf("Begrenzung %d %n",longestReiheBegrenzung);
+            
+           if (longestReiheBegrenzung > 0) {
+            for (int j = 0; j <= longestReiheBegrenzung-testedFor; j++) {
                  
                 for (int i = 0; i < musterSiteLength; i++) {
-                    sum += theArray[startPosition][i+j+1];
-                    sum += theArray[startPosition+testedFor-1][i+j+1];
-                }
-                
+                    sum += theArray[startPosition][i+j+1]; System.out.printf("+ %d (%d) +", theArray[startPosition][i+j+1], j);
+                    sum += theArray[startPosition+testedFor-1][i+j+1]; System.out.printf("+ %d (%d) +", theArray[startPosition+testedFor-1][i+j+1], j);
+                }// for inside for and if(musterSiteLength)
+            }//for 
+            for (int j = 0; j <= longestReiheBegrenzung-testedFor; j++) {
                 for (int i = 0; i < musterSiteHeigth; i++ ) {
-                    sum += theArray[startPosition+1+i][j];
-                    sum += theArray[startPosition+1+i][j+testedFor-1];
-                }
+                    sum += theArray[startPosition+1+i][j]; System.out.printf("+ %d (%d) +", theArray[startPosition+1+i][j], j);
+                    sum += theArray[startPosition+1+i][j+testedFor-1]; System.out.printf("+ %d (%d) +", theArray[startPosition+1+i][j+testedFor-1], j);
+                }// for inside for and if (musterSiteHeigth)
                 
-             }
-         //  }
-           startPosition++;
-           longestReihe = 0;
-           longestReiheToCompare = 0;
+             }//for inside if
+           }//if
         }
+           startPosition++;
+           longestReiheToCompare = 0;
+           longestReiheBegrenzung = 0;
+           longestReiheMid = Integer.MAX_VALUE; 
+           longestReiheStartEnd = Integer.MAX_VALUE;
+           System.out.println();
+         
+        }//while
         
-        
+        /*  for (int i = startPosition; i<testedFor+startPosition; i++) {
+        longestReiheToCompare = theArray[i].length;
+        if (longestReiheToCompare>longestReiheMid) { longestReiheMid = longestReiheToCompare; 
+       }
+    }
+    for (int i = startPosition; i<testedFor+startPosition; i++) {
+        longestReiheToCompare = theArray[i].length;
+        if (longestReiheToCompare<longestReiheMid) { longestReiheMid = longestReiheToCompare; 
+       }
+    }
+    */
         
         
         
