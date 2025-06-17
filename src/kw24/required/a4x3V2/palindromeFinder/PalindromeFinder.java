a5df7ca827e4f36abb6101d99f3d67396c18eac3
Zadoya <zadoyamaxim@gmail.com>
2025-06-17 23:56:46 +0200

    A4 Final

--- src/kw24/required/a4x3V2/palindromeFinder/PalindromeFinder.java
+++ src/kw24/required/a4x3V2/palindromeFinder/PalindromeFinder.java
@@ -29,13 +29,13 @@ public class PalindromeFinder {
 
         while (aktuelleLangeMuster >= 1) {
             for (int i = 0; i <= stringToCheck.length() - aktuelleLangeMuster; i++) {
+                
                 int left = i;
                 int right = aktuelleLangeMuster - 1 + i;
                 boolean isSubstringPalindrome = true;
 
                 while (left < right) {
-                    if (Character.toLowerCase(stringToCheck.charAt(left)) != Character
-                            .toLowerCase(stringToCheck.charAt(right))) {
+                    if (Character.toLowerCase(stringToCheck.charAt(left)) != Character.toLowerCase(stringToCheck.charAt(right))) {
                         isSubstringPalindrome = false;
                         break;
                     }
